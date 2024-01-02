5adee9f331efcdb7a2405aa40e05156acd170857
mewoodland <mewoodland@mdanderson.org>
2024-01-02 13:13:39 -0600

    add docker capabilities

---
+++ utils/preprocess_msd.py
@@ -0,0 +1,26 @@
+"""
+Unzip the MSD datasets.
+"""
+
+# Imports
+import argparse
+import os
+import tarfile
+import tqdm
+
+# Arguments
+parser = argparse.ArgumentParser()
+parser._action_groups.pop()
+required = parser.add_argument_group('Required Arguments')
+required.add_argument('--in_dir', required=True, type=str, help='Path to directory with tar files.')
+required.add_argument('--out_dir', required=True, type=str, help='Path to directory to put unzipped files into.')
+args = parser.parse_args()
+in_dir = args.in_dir
+out_dir = args.out_dir
+if not os.path.exists(out_dir):
+    os.mkdir(out_dir)
+
+# Main Code
+for tar_file in tqdm.tqdm(os.listdir(in_dir)):
+    with tarfile.open(os.path.join(in_dir, tar_file)) as tar:
+        tar.extractall(path=out_dir)
